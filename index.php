b4d2de95a11391f98b09516ef03cf1970830e5f1
Michalis Mastoros <128923@student.drenthecollege.nl>
2021-12-06 14:03:00 +0100

    your msg

---
+++ index.php
@@ -0,0 +1,13 @@
+</html>
+  <head>
+    <meta charset="utf-8">
+    <title>index</title>
+  </head>
+  <body>
+    <h1>Index Page</h1>
+    <a href="onepiece.html">One Piece</a><br>
+    <a href="thewitcher.html">The Witcher</a><br>
+    <a href="mgsv.html">Metal Gear Solid V</a><br>
+    <a href="linus.html">Linus Torvalds</a>
+  </body>
+</html>
